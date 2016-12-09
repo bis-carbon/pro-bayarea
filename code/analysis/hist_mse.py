e9f9f4790e1dd3777fd42537b4bc204bf0f41361
Steve Shimozaki <Steve Shimozaki>
2016-12-09 15:51:56 -0800

    remove pptxs, add readmes, added new hist_mse

--- code/analysis/hist_mse.py
+++ code/analysis/hist_mse.py
@@ -9,10 +9,10 @@ import numpy as np
 import pandas as pd
 
 #data_dir ='C:/Users/ibshi/Desktop/startup.ml/video_stuff/prednet-master/kitti_results_50hz/'
-data_dir ='./kitti_results_50hz/'
-prefix = 'mse_frame2_P20_'
-parts = 20
-num_clips_per_avi = 20
+data_dir ='./shanghai_results_2hz/'
+prefix = 'mse_frame2_P1_'
+parts = 1
+num_clips_per_avi = 1
 num_pred_frames_per_clip = 9 # remember, no prediction for first frame 
 
 for part in range(1,parts+1):
@@ -29,22 +29,21 @@ mse_clip = np.apply_over_axes(np.mean, mse_frame, [1])
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-overall = plt.hist(mse_clip)
-plt.xlabel('MSE')
-plt.ylabel('Frequency out of 2160 clips')
-plt.title('Over all 9 frames')
-pmean = np.mean(mse_clip)
-psd = np.std(mse_clip)
-cap_str = 'Mean = '+str(pmean)+', SD = '+str(psd)
-plt.figtext(0.0,0.0,cap_str)
-plt.show()
-plt.savefig('MSEoverall.png')
-plt.close()
+do_scale = 1
+if (do_scale):
+    xmax = np.max(np.max(mse_frame))
+    ymax, tmp = mse_frame.shape
 
 for frame in range(0,num_pred_frames_per_clip):
     frame_plot = plt.hist(mse_frame[:][frame])
+    if (do_scale):
+        head = 'MSEsc'
+        plt.axis([0,xmax,0,ymax])
+    else:
+        head = 'MSE'
     plt.xlabel('MSE')
-    plt.ylabel('Frequency out of 2160 clips')
+    curr_ylabel = 'Frequency out of '+str(int(2160*parts/20))+ ' clips'
+    plt.ylabel(curr_ylabel)
     tit_str = 'For ' + 'frame'+ str(frame + 2) 
     plt.title(tit_str)
     pmean = np.mean(mse_frame[:][frame])
@@ -52,8 +51,26 @@ for frame in range(0,num_pred_frames_per_clip):
     cap_str = 'Mean = '+str(pmean)+', SD = '+str(psd)
     plt.figtext(0.0,0.0,cap_str)
     plt.show()
-    figfile = 'MSE'+str(frame + 2)+'.png'
+    figfile = head+str(frame + 2)+'.png'
     plt.savefig(figfile)
     plt.close()
-    
-    
+
+
+overall = plt.hist(mse_clip)
+if (do_scale):
+    plt.axis([0,xmax,0,ymax])
+    overall_file = 'MSEsc_overall.png'
+else:
+    overall_file = 'MSE_overall.png'
+plt.xlabel('MSE')
+curr_ylabel = 'Frequency out of '+str(int(2160*parts/20))+ ' clips'
+plt.ylabel(curr_ylabel)
+plt.title('Over all 9 frames')
+pmean = np.mean(mse_clip)
+psd = np.std(mse_clip)
+cap_str = 'Mean = '+str(pmean)+', SD = '+str(psd)
+plt.figtext(0.0,0.0,cap_str)
+plt.show()
+plt.savefig(overall_file)
+plt.close()    
+  
